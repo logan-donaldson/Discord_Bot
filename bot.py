569593bd29349c6c5a8c4d243fde2639d9029f2d
logan-donaldson <logan.r.donaldson@gmail.com>
2021-08-09 22:39:42 -0400

    Personal Discord bot coded in python

---
+++ bot.py
@@ -0,0 +1,318 @@
+import os
+import discord
+import random
+import bs4
+import re
+
+from botData import hangmanPics, hangmanWords, jokes
+from dotenv import load_dotenv
+from discord.ext import commands
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+
+#intialize bot for desired server
+intents = discord.Intents.all()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+bot = commands.Bot(command_prefix='!')
+
+#randomly sends a joke from an array of jokes
+@bot.command(name='joke', help='Responds with a witty one-liner')
+async def joke(ctx):
+    response = random.choice(jokes)
+    await ctx.send(response)
+
+#simulates dice rolling, arg1 (optional) = number of dice to roll, arg2 (optional) = number of sides on each dice
+@bot.command(name='roll_dice', help='Simulates rolling dice. After !roll_dice type # of dice to roll and the # of sides on each dice i.e. !roll_dice 2 3')
+async def roll(ctx, *args):
+    dice = []
+    
+    if(len(args) >= 2):
+        for i in range(int(args[0])):
+            dice.append(str(random.choice(range(1, int(args[1]) + 1))))
+    elif(len(args) == 1):
+        for i in range(int(args[0])):
+            dice.append(str(random.choice(range(1, 7))))
+    else:
+        dice.append(str(random.choice(range(1, 7))))
+    
+    await ctx.send(', '.join(dice))
+
+#sends a gif of the infamous Rick Astley music video
+@bot.command(name='rickroll', help="Just Try It")
+async def rickRoll(ctx):
+    await ctx.send("https://media.giphy.com/media/lgcUUCXgC8mEo/giphy.gif")
+
+#scrapes tenor.com for a relvant gif and sends it, arg1 = the search term with spaces replaced by _
+@bot.command(name='gif', help="After !gif type what you want a gif of making sure to replace spaces with _ i.e. !gif call_of_duty" +
+                                   "\n You may also include a number to see a different related gif i.e. !gif call_of_duty 6")
+async def searchTenorGif(ctx, *args):
+    
+    searchTerm = args[0].replace('_', '-')
+    num = 1
+    if(len(args) == 2):
+        num = int(args[1])
+
+    my_url = "https://tenor.com/search/" + searchTerm
+    uClient = urlopen(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    soupp = soup(page_html, "html.parser")
+
+    container = soupp.find_all('div', {'class':'Gif'})
+   
+    if(num >= len(container)):
+        await ctx.send("There are not that many gifs!")
+        return
+
+    for gif in container[num-1].find_all('img'):
+        gifSource = gif.get('src')
+        if(gifSource[-3:] != "gif"):
+              continue
+        else:
+            await ctx.send(gifSource)
+            return
+
+#scrapes tenor.com for a relvant sticker (transparent gif) and sends it, arg1 = the search term with spaces replaced by _
+@bot.command(name='sticker', help="After !sticker type what you want a sticker of making sure to replace spaces with _ i.e. !sticker call_of_duty" +
+                                   "\n You may also include a number to see a different related sticker i.e. !sticker call_of_duty 6")
+async def searchTenorSticker(ctx, *args):
+    
+    searchTerm = args[0].replace('_', '-')
+    num = 1
+    if(len(args) >= 2):
+        num = int(args[1])
+
+    my_url = "https://tenor.com/search/" + searchTerm
+    uClient = urlopen(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    soupp = soup(page_html, "html.parser")
+
+    container = soupp.find_all('div', {'class':'Sticker'})
+
+    if(num >= len(container)):
+        await ctx.send("There are not that many stickers!")
+        return
+
+    for sticker in container[num-1].find_all('img'):
+        stickerSource = sticker.get('src')
+        if(stickerSource[-3:] != "gif"):
+            continue
+        else:
+            await ctx.send(stickerSource)
+            return
+
+@bot.event
+async def on_message(message):
+
+    #prevent bot from responding to itself
+    if message.author == bot.user:
+        return
+
+    #wishs a happy birthday
+    if "happy birthday" in message.content.lower():
+        await message.channel.send('Happy Birthday! - Soap Bot')
+        return
+
+    #reacts to praise
+    if message.content == "good bot":
+        await message.add_reaction('\N{HEAVY BLACK HEART}')
+        return
+
+    #reacts to criticism
+    if message.content == "bad bot":
+        await message.add_reaction('😢')
+        return
+
+    await bot.process_commands(message)
+
+#traverses wikipedia by following the first link on each page until the Wikipedia Page for Philosphy is found
+@bot.command(name='wiki', help="Traverses Wikipedia by clicking on the first link on each page until Philosophy is reached. After !wiki enter the Wikipedia page you would like to start from, make sure the page exists and is formatted correctly i.e. " +
+"if you wanted to start at https://en.wikipedia.org/wiki/Sonic_the_Hedgehog you would type !wiki sonic_the_hedgehog")
+async def findPhilosophy(ctx, startingURL):
+    
+    def findNextLink(wikiURL, visited, forbiddenLinks, forbiddenComponents):
+
+        soupp = getHTML(wikiURL)
+        container = soupp.find_all('p')
+        urlStub = "https://en.wikipedia.org"
+
+        for paragraph in container:
+            links = paragraph.find_all('a')
+            for link in links:
+                if validStub(str(link), forbiddenComponents):
+                    startIndex = str(link).find("href")
+                    link = re.findall(r'["](.*?)["]', str(link)[startIndex:])
+                    fullURL = str(urlStub) + str(link[0])
+                    if validURL(fullURL, visited, forbiddenLinks):
+                        return fullURL
+
+    def getHTML(wikiURL):
+        uClient = urlopen(wikiURL)
+        page_html = uClient.read()
+        uClient.close()
+        return soup(page_html, "html.parser")
+
+    def validStub(stub, forbiddenComponents):
+        for component in forbiddenComponents:
+            if component in stub:
+                return False
+        if "href=\"/wiki/" not in stub:
+            return False
+        return True
+
+    def validURL(fullURL, visited, forbiddenLinks):
+        if fullURL not in visited and fullURL not in forbiddenLinks:
+            return True
+        else:
+            return False
+
+    startingURL = "https://en.wikipedia.org/wiki/" + startingURL
+    pageName = [startingURL[30:]]
+    visited = [startingURL]
+    forbiddenLinks = ["https://en.wikipedia.org/wiki/Wikipedia:NOTRS",
+    "https://en.wikipedia.org/wiki/Wikipedia:No_original_research"]
+    forbiddenComponents = ["Help:","img","#","Coordinates", "Wikipedia:"]
+
+    while visited[len(visited)-1] != "https://en.wikipedia.org/wiki/Philosophy":
+        newURL = findNextLink(visited[len(visited)-1], visited, forbiddenLinks, forbiddenComponents)
+        visited.append(newURL)
+        pageName.append(newURL[30:])
+
+    visitedString = ", ".join(pageName)
+    await ctx.send("THE PATH TO PHILOSOPHY: " + visitedString)
+
+
+#ASCII art came courtesy of chrishorton on github, https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
+#Play hangman against the bot, intialize game by calling command with no argument, make guesses by calling game with argument
+@bot.command(name='hm', help="To play hang man: 1.) !hm 2.) !hm letter/guess")
+async def playHM(ctx, *args):
+    
+    global word, guesses, revealed, numIncorrect, numCorrect
+    
+    #initialize new game
+    if(len(args) == 0):
+        guesses = []
+        word = random.choice(words).lower()
+        revealed = []
+        numIncorrect = 0
+        numCorrect = 0
+        await ctx.send(HANGMANPICS[0] + '\nWord: ' + '—' * len(word))
+        return
+
+    #checks if the letter/word has already been guessed
+    guess = args[0].lower()
+    if guess in guesses:
+        await ctx.send("You already guessed that letter")
+        return
+
+    #if guess is a letter
+    if len(guess) == 1:
+        if guess in word.lower():
+            numCorrect = numCorrect + word.count(guess)
+            if numCorrect == len(word):
+                await ctx.send("YOU WIN!")
+                return
+            revealed.append(guess)
+            guesses.append(guess)
+        else:
+            numIncorrect = numIncorrect + 1
+            if numIncorrect == 6:
+                await ctx.send(HANGMANPICS[6] + "\nYOU LOSE!")
+                return
+            guesses.append(guess)
+
+    #if guess is a word
+    if len(guess) != 1:
+        if guess == word:
+            await ctx.send("YOU WIN!")
+            return
+        else: 
+            numIncorrect = numIncorrect + 1
+            guesses.append(guess)
+
+    #construct string representing current game state
+    message = HANGMANPICS[numIncorrect] + '\nWord: '
+    for letter in word:
+        if letter in revealed:
+            message = message + letter
+        else:
+            message = message + '—'
+    message = message + '\nGuesses: ' + (', '.join(guesses))
+
+    await ctx.send(message)
+    return
+
+
+@bot.command(name='e3', help="")
+async def e3Update(ctx, *args):
+    
+    myURL = "https://www.pcgamer.com/E3-games-2021/"
+    uClient = urlopen(myURL)
+    page_html = uClient.read()
+    uClient.close()
+    soupp = soup(page_html, "html.parser")
+
+    games = soupp.find_all("h3", {"class" : "article-body__section"})
+    moreGames = soupp.find_all("ul")
+    moreGames = moreGames[5]
+    moreGames = moreGames.find_all("li")
+
+    gamesArray = []
+    for game in games:
+        gamesArray.append(game.text.strip())
+    for game in moreGames:
+        gamesArray.append(game.text.strip())
+
+
+    paragraphs = soupp.find_all("p")
+    info = []
+    for paragraph in paragraphs:
+        if "What is it?" in paragraph.text:
+            index = str(paragraph.text).rfind("\n")
+            paragraph = str(paragraph.text)[:index]
+            info.append(paragraph)
+    
+    for i in range(43,0,-1):
+        info.pop(len(info) - i)
+
+    links = []
+    for game in games:
+        if "allowfullscreen" in str(game.nextSibling()):
+            startIndex = str(game.nextSibling()).find("data-lazy-src=\"")
+            endIndex = str(game.nextSibling()).find(">")
+            link = str(game.nextSibling())[startIndex + 15: endIndex - 1]
+            link = link.replace("embed/", "watch?v=")
+            links.append(link)
+        else:
+            links.append("No Trailer")
+
+    for game in moreGames:
+        startIndex = str(game).find("\"")
+        endIndex = str(game).find("/\"")
+        link = str(game)[startIndex + 1 : endIndex]
+        links.append(str(link))
+
+
+    for i in range(len(links)):
+        if i < len(info):
+            info[i] = str(info[i]) + "\n" + str(links[i]) + "\n"
+        else: 
+            info.append(" \n" + str(links[i]) + " \n")
+
+    dict = {gamesArray[i]: info[i] for i in range(len(gamesArray))}
+
+    if len(args) == 0:
+        string = ""
+        for key in dict.keys():
+            string = string + ", " + key
+        await ctx.send(string)
+    else:
+        await ctx.send(dict.get(args[0].replace(' ','_')))
+
+
+
+
+bot.run(TOKEN)
---
+++ botData.py
@@ -0,0 +1,95 @@
+hangmanPics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+hangmanWords = ['Sonic','Mario','Luigi','Kirby','Controller','Keyboard','Playstation','Xbox','Nintendo','Switch','Steam','Borderlands']
+
+jokes = [
+        'My wife told me to stop impersonating a flamingo. I had to put my foot down. ',
+        'I went to buy some camo pants but couldn’t find any.',
+        'I failed math so many times at school, I can’t even count.',
+        'I used to have a handle on life, but then it broke.',
+        'I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.',
+        'I heard there were a bunch of break-ins over at the parking garage. That is wrong on so many levels.',
+        'Don’t you hate it when someone answers their own questions? I do.',
+        'It takes a lot of balls to golf the way I do.',
+        'I told him to be himself; that was pretty mean, I guess.',
+        'I can’t believe I got fired from the calendar factory. All I did was take a day off.',
+        'Never trust atoms; they make up everything.',
+        'I was addicted to the hokey pokey, but then I turned myself around.',
+        'I used to think I was indecisive. But now I’m not so sure.',
+        'Russian dolls are so full of themselves.',
+        'My therapist says I have a preoccupation for revenge. We’ll see about that.',
+        'Two fish are in a tank. One says, ‘How do you drive this thing?’',
+        'Just burned 2,000 calories. That’s the last time I leave brownies in the oven while I nap.',
+        'Always borrow money from a pessimist. They’ll never expect it back.',
+        'I’m reading a book about anti-gravity. It’s impossible to put down.',
+        'Atheism is a non-prophet organization.',
+        'The future, the present, and the past walk into a bar. Things got a little tense.',
+        'Maybe if we start telling people their brain is an app, they’ll want to use it.',
+        'If a parsley farmer gets sued, can they garnish his wages?',
+        'I didn’t think orthopedic shoes would help, but I stand corrected.',
+        'People who take care of chickens are literally chicken tenders.',
+        'It was an emotional wedding. Even the cake was in tiers.',
+        '6:30 is the best time on a clock, hands down.',
+        'Refusing to go to the gym is a form of resistance training.',
+        'One of the cows didn’t produce milk today. It was an udder failure.',
+        'Adam & Eve were the first ones to ignore the Apple terms and conditions.',
+        'The man who invented Velcro has died. RIP.',
+        'Despite the high cost of living, it remains popular.',
+        'The rotation of Earth really makes my day.',
+        'Are people born with photographic memories, or does it take time to develop?',
+        'Geology rocks, but geography’s where it’s at.',
+        'No',
+        'Don\'t tell me what to do'
+    ]
+
+
+
